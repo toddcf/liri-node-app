28eece419f3f4f7657f6754c51fd7afde79ae3a1
Todd Croak-Falen <Todd Croak-Falen>
2016-09-04 23:49:42 -0700

    Added liri.js file.

---
+++ js/liri.js
@@ -0,0 +1,13 @@
+// Write the code you need to grab the data from keys.js.
+
+// Then store the keys in a variable.
+
+// Make it so liri.js can take one of the following commands:
+
+/** `my-tweets`
+
+* `spotify-this-song`
+
+* `movie-this`
+
+* `do-what-it-says`*/
